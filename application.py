6c0b3d9a6fa117f52de5e3b2b933f9ee18c88ecc
abhijeet malik <abma00001@stud-uni.saarland.de>
2026-02-25 12:36:29 +0100

    added the python config for deployment

--- application.py
+++ application.py
@@ -58,4 +58,9 @@ def about():
 
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0',port = 5001,debug = True)
+    app.run(host = '0.0.0.0',port = 5000,debug = True)
+
+
+
+
+     #to run on the local server http://127.0.0.1:8000
